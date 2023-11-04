73a90cc2e92d53e1702adb83258d99f9720e9da8
François Martin <f.martin@fastmail.com>
2023-11-04 17:45:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Cloudflare WAF to AbuseIPDB
+
+## Summary
+
+This is a Python script that queries Cloudflare's firewall event logs and reports any potentially malicious IP addresses to AbuseIPDB, a database that tracks IPs associated with malicious activities.
+
+The script first imports several libraries: `json`, `requests`, `time`, `os`, `yaml`, and `sys`.
+
+`json` is used for encoding and decoding JSON data, which is used by the Cloudflare and AbuseIPDB APIs.
+
+`requests` is used to make HTTP requests to the Cloudflare and AbuseIPDB APIs.
+
+`time` is used to get the current time and format it for use in the query payload.
+
+`os` and `sys` are used to load configuration data from a YAML file or command line arguments.
+
+The `load_config` function reads a YAML file and returns a dictionary of the file's contents.
+
+If a `config.yml` file exists in the current directory, the script loads configuration data from it. If the file does not exist, the script expects to receive four command line arguments: `CLOUDFLARE_ZONE_ID`, `CLOUDFLARE_EMAIL`, `CLOUDFLARE_API_KEY`, and `ABUSEIPDB_API_KEY`.
+
+The script then constructs a payload containing a GraphQL query that filters Cloudflare's firewall event logs for potentially malicious events that occurred within the last 8.5 hours. The payload includes Cloudflare's `CLOUDFLARE_ZONE_ID`, `CLOUDFLARE_EMAIL`, and `CLOUDFLARE_API_KEY` for authentication. The payload is sent as a `JSON` string to the Cloudflare API.
+
+The script defines a function `get_blocked_ip` that sends the payload to the Cloudflare API and returns a list of potentially malicious IP addresses. The function retries the API call up to 60 times, with a 1 second pause between each attempt, before giving up.
+
+The script defines a function `get_comment` that takes a dictionary containing information about a potentially malicious IP address and returns a string that describes the IP address and associated details for reporting to AbuseIPDB.
+
+The script defines a function `report_bad_ip` that takes a dictionary containing information about a potentially malicious IP address, constructs a payload containing the IP address and associated details, and sends the payload to the AbuseIPDB API to report the IP address as potentially malicious.
+
+The script prints a message indicating that it has started and the current time. It then calls the `get_blocked_ip` function to retrieve a list of potentially malicious IP addresses from Cloudflare's firewall event logs. If the function returns a non-empty list, the script calls the `report_bad_ip` function for each IP address in the list, excluding any IP addresses associated with a specific rule ID. The script prints a message indicating the number of potentially malicious IP addresses found in the event logs.
+
+## How to use for yourself
+
+Don't fork this repo - that's not how this is designed to be used. Instead, select "Use this template", then "Create new repository".
+
+# First, Enable and Configure GitHub Actions：
+
+If you don't configure these, you'll stare at errors for eternity wondering where you're fucking up.
+After you create a new repository thru "Use This Template", go into the repository settings, then go to "Secrets", then add the following things with the following names. 
+
+- `CLOUDFLARE_ZONE_ID`: Cloudflare ZONE ID
+- `CLOUDFLARE_API_KEY`: Cloudflare API Key
+- `CLOUDFLARE_EMAIL`: Cloudflare Email
+- `ABUSEIPDB_API_KEY`: AbuseIPDB API Key
+
+  After this, modify the name of your `report.yml` workflow to make the repository name match YOUR repository name. 
+
+**PLEASE READ THIS:** Before you enable this for the first time and allow it to start reporting, REVIEW YOUR WAF SETTINGS. This worker will report your firewall events overall, so if you have a configuration that causes requests to generate logs for no reason, OR a specific security setting that issues Managed Challenges regardless of condition, then you'll equally start reporting random IP's for no reason. If you do this, your AbuseIPDB key will be revoked, and your account could be locked and/or terminated. 
+
+DO NOT TURN THIS ON IN IT'S DEFAULT EXISTANCE IF YOUR WAF CONFIG IS FUCKING OBNOXIOUS
+YOU WILL RUIN IT FOR EVERYONE AND CAUSE GENERAL TECHNICAL MAYHEM.
+
+## Related
+
+[AbuseIPDB-to-Cloudflare-WAF](https://github.com/MHG-LAB/AbuseIPDB-to-Cloudflare-WAF)
+
+## Support
+
+[AbuseIPDB](https://www.abuseipdb.com/) : AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks
+
+[Cloudflare](https://www.cloudflare.com/)
+
+[Cloudflare Block Bad Bot Ruleset](https://github.com/XMD0718/cloudflare-block-bad-bot-ruleset)
+
+## AbuseIPDB Contributor
+
+<a href="https://www.beehive.systems" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
+	<img src="https://www.abuseipdb.com/contributor/102055.svg" alt="AbuseIPDB Contributor Badge" style="width: 781px;border-radius: 5px;border-top: 5px solid #058403;border-right: 5px solid #111;border-bottom: 5px solid #111;border-left: 5px solid #058403;padding: 5px;background: #35c246 linear-gradient(rgba(255,255,255,0), rgba(255,255,255,.3) 50%, rgba(0,0,0,.2) 51%, rgba(0,0,0,0));padding: 5px;box-shadow: 2px 2px 1px 1px rgba(0, 0, 0, .2);">
+</a>
+
+This number is tied to the code in this very repo.
+
+If this number is going up, ours is working, even if yours doesn't seem to be.
+
+Consider yourself an...unsmooth operator.
+
+## License
+
+[MIT](https://github.com/MHG-LAB/Cloudflare-WAF-to-AbuseIPDB/blob/main/LICENSE)
---
+++ main.py
@@ -0,0 +1,176 @@
+# This code was documented by the BeeHive Team, to help those seeking to learn.
+# This documentation does not impact the function of the code.
+# Please do not remove it, so that users who reuse or find this can learn.
+
+# Importing required libraries
+import json
+import requests
+import time
+import os
+import yaml
+import sys
+
+# Define function to load configuration
+def load_config(path):
+    # Open file for read
+  f = open(path, 'r', encoding='utf-8')
+  # Read file contents
+  ystr = f.read()
+  # Load YAML string into Python object
+  ymllist = yaml.load(ystr, Loader=yaml.FullLoader)
+  # Return loaded configuration
+  return ymllist
+
+# Check if configuration file exists
+if os.path.exists('config.yml'):
+    # If configuration file exists, load it
+  c=load_config('config.yml')
+  # Set Cloudflare API Secret References
+  CLOUDFLARE_ZONE_ID = c['CLOUDFLARE_ZONE_ID']
+  CLOUDFLARE_EMAIL = c['CLOUDFLARE_EMAIL']
+  CLOUDFLARE_API_KEY = c['CLOUDFLARE_API_KEY']
+  # Set AbuseIPDB API Key
+  ABUSEIPDB_API_KEY = c['ABUSEIPDB_API_KEY']
+else:
+    # If configuration file does not exist, get credentials from command line.
+  CLOUDFLARE_ZONE_ID = sys.argv[1]
+  CLOUDFLARE_EMAIL = sys.argv[2]
+  CLOUDFLARE_API_KEY = sys.argv[3]
+  ABUSEIPDB_API_KEY = sys.argv[4]
+
+# Set payload for Cloudflare API requests
+PAYLOAD={
+  "query": """query ListFirewallEvents($zoneTag: string, $filter: FirewallEventsAdaptiveFilter_InputObject) {
+    viewer {
+      zones(filter: { zoneTag: $zoneTag }) {
+        firewallEventsAdaptive(
+          filter: $filter
+          limit: 2500
+          orderBy: [datetime_DESC]
+        ) {
+          action
+          clientASNDescription
+          clientAsn
+          clientCountryName
+          clientIP
+          clientRequestHTTPHost
+          clientRequestHTTPMethodName
+          clientRequestHTTPProtocol
+          clientRequestPath
+          clientRequestQuery
+          datetime
+          rayName
+          ruleId
+          source
+          userAgent
+        }
+      }
+    }
+  }""",
+  "variables": {
+    "zoneTag": CLOUDFLARE_ZONE_ID,
+    "filter": {
+      "datetime_geq": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.localtime(time.time()-60*60*8-60*60*2.5)),
+      "datetime_leq": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.localtime(time.time()-60*60*8)),
+      # "OR":[{"action": "block"}, {"action": "managed_challenge"}, {"action": "jschallenge"}],
+      "AND":[
+          {"action_neq": "allow"},
+          {"action_neq": "challenge_solved"},
+          {"action_neq": "challenge_failed"},
+          {"action_neq": "challenge_bypassed"},
+          {"action_neq": "jschallenge_solved"},
+          {"action_neq": "jschallenge_failed"},
+          {"action_neq": "jschallenge_bypassed"},
+          {"action_neq": "managed_challenge_skipped"},
+          {"action_neq": "managed_challenge_non_interactive_solved"},
+          {"action_neq": "managed_challenge_interactive_solved"},
+          {"action_neq": "managed_challenge_bypassed"},
+      ]
+    }
+  }
+}
+# Convert PAYLOAD dictionary to a JSON string
+PAYLOAD = json.dumps(PAYLOAD)
+# Define headers for the API request
+headers={"Content-Type":"application/json","X-Auth-Key":CLOUDFLARE_API_KEY,"X-Auth-Email":CLOUDFLARE_EMAIL}
+# Set the initial time to live value to 60
+ttl=60
+# Define a function to get a list of blocked IP Addresses
+def get_blocked_ip():
+    # Access global variable ttl
+  global ttl
+  # Decrement ttl by 1
+  ttl=ttl-1
+  # Print the current value of TTL
+  print("ttl:",ttl)
+  # If TTL reaches 0, return an empty list
+  if ttl<=0:
+    return []
+  try:
+      # Send a POST request to the Cloudflare API with the defined headers and PAYLOAD data
+    r=requests.post("https://api.cloudflare.com/client/v4/graphql/",headers=headers,data=PAYLOAD)
+    # If the response is None, call the function recursively
+    if str(type(r.json())) == "<class 'NoneType'>":
+      get_blocked_ip()
+    else:
+        # Otherwise return JSON response data
+      return r.json()
+  except Exception as e:
+      # If there is an exception, call the function recursively
+    get_blocked_ip()
+
+# Define a function to generate a comment for the Bad IP Address report intended for AbuseIPDB
+def get_comment(it):
+  return "Threat Blocked by BeeHive from (ASN:"+it['clientAsn']+") (Network:"+it['clientASNDescription']+") (Host:"+it['clientRequestHTTPHost']+") (Method:"+it['clientRequestHTTPMethodName']+") (Protocol:"+it['clientRequestHTTPProtocol']+") (Timestamp:"+it['datetime']+")"
+
+# Define a function to report a bad IP address to AbuseIPDB
+def report_bad_ip(it):
+  try:
+    url = 'https://api.abuseipdb.com/api/v2/report'
+    params = {
+      'ip': it['clientIP'],
+      'categories': '9,13,14,15,16,19,20,21',
+      'comment': get_comment(it)
+    }
+    headers = {
+      'Accept': 'application/json',
+      'Key': ABUSEIPDB_API_KEY
+    }
+    # Send a POST request to the AbuseIPDB API with the required contents
+    r=requests.post(url=url, headers=headers, params=params)
+    if r.status_code==200:
+        # If response code 200, record a successfully reported IP
+      print("reported:",it['clientIP'])
+    else:
+        # Otherwise, print the status code as an error
+      print("error:",r.status_code)
+      # Parse the response data and print it
+    decodedResponse = json.loads(r.text)
+    print(json.dumps(decodedResponse, sort_keys=True, indent=4))
+  except Exception as e:
+      # If there is an exception, print the needed error message to account for it
+    print("error:",e)
+
+# Define a list of excluded Cloudflare WAF Rule IDs
+excepted_ruleId = ["fa01280809254f82978e827892db4e46"]
+
+# Print start time and end time within output
+print("==================== Start ====================")
+print(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+print(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()-60*60*8))))
+a=get_blocked_ip()
+print(str(type(a)))
+if str(type(a)) == "<class 'dict'>" and len(a)>0:
+  ip_bad_list=a["data"]["viewer"]["zones"][0]["firewallEventsAdaptive"]
+  print(len(ip_bad_list))
+
+
+  reported_ip_list=[]
+  for i in ip_bad_list:
+    if i['ruleId'] not in excepted_ruleId:
+      if i['clientIP'] not in reported_ip_list:
+        report_bad_ip(i)
+        reported_ip_list.append(i['clientIP'])
+
+  print(len(reported_ip_list))
+print("==================== End ====================")
